42c933c78d166617a515bb7be65e09d3b6f829da
Gene Blanchard <gblanchard4@gmail.com>
2014-09-30 17:45:08 -0500

    HTML ALL THE THINGS

--- secondary_documentation.py
+++ secondary_documentation.py
@@ -2,12 +2,14 @@
 
 import argparse
 import os
+import markdown
 
 __author__ = "Gene Blanchard"
 __email__ = "me@geneblanchard.com"
 
 '''
 DOC:
+Requires: python-markdown
 
 
 Secondary analysis folder structure
@@ -70,13 +72,16 @@ def make_header(analysis_folder):
 def make_secondary_folder_list(analysis_folder):
 	secondary_folder_list = []
 	for secondary_folder in list_subdirectories(analysis_folder):
-		folder_bullet = "* [{}](#{})  \n".format(secondary_folder,secondary_folder.lower())
+		filepath =  analysis_folder+'/'+secondary_folder
+		folder_bullet = "* #####[{}]({}/index.html)  \n".format(secondary_folder,filepath)
 		secondary_folder_list.append(folder_bullet)
 	return secondary_folder_list 
 
-def make_secondary_folder(analysis_folder):
-	string_list = []
+def make_secondary_folder_index(analysis_folder):
+	
 	for secondary_folder in list_subdirectories(analysis_folder):
+		string_list = ['<link href="file://{}/github-markdown.css" rel="stylesheet"></link>  \n'.format(analysis_folder)]
+		#string_list = ['<link href="http://kevinburke.bitbucket.org/markdowncss/markdown.css" rel="stylesheet"></link>  \n']
 		name = secondary_folder
 		filepath = analysis_folder+'/'+name
 	
@@ -86,7 +91,7 @@ def make_secondary_folder(analysis_folder):
 		taxa_list = []
 
 		if os.path.isdir(filepath):
-			string_list.append("---\n## {0}  \n#### [Click To Open Analysis Folder In Browser]({1})  \n".format(name, filepath))
+			string_list.append("## {0}  \n---  \n#### [Click To Open Analysis Folder In Browser]({1})  \n".format(name, filepath))
 
 			# Stats table
 			string_list.append(biom_summary_to_table(filepath))
@@ -95,10 +100,10 @@ def make_secondary_folder(analysis_folder):
 
 			# Alpha diversity
 			alpha_list.append("#### Alpha Diversity\n")
-			alpha_graphic = os.listdir("{}/alpha_diversity/alpha_rarefaction_plots/average_plots/".format(filepath))[0]
+			alpha_graphic = [png for png in os.listdir("{}/alpha_diversity/alpha_rarefaction_plots/average_plots/".format(filepath)) if png.startswith('observed_species')][0]
 			alpha_graphic_path = "{}/alpha_diversity/alpha_rarefaction_plots/average_plots/{}".format(filepath, alpha_graphic)
-			alpha_list.append("![Alpha Plot](file://{} 'Example Alpha Plot')  \n  \n".format(alpha_graphic_path))
-			alpha_list.append("* [Rarefaction Plots](file://{}/alpha_diversity/alpha_rarefaction_plots/rarefaction_plots.html)  \n".format(filepath))
+			alpha_list.append("[![Alpha Plot](file://{} 'Example Alpha Plot')](file://{}/alpha_diversity/alpha_rarefaction_plots/rarefaction_plots.html)  \n  \n".format(alpha_graphic_path, filepath))
+			#alpha_list.append("* [Rarefaction Plots](file://{}/alpha_diversity/alpha_rarefaction_plots/rarefaction_plots.html)  \n".format(filepath))
 
 			# Taxa summary
 			taxa_list.append("#### Taxa Summary  \n")
@@ -125,16 +130,19 @@ def make_secondary_folder(analysis_folder):
 					if beta_folder.startswith('weighted_'):
 						beta_3d_list.append("* [Weighted Unifrac](file://{0}/beta_diversity/weighted_unifrac_emperor_pcoa_plot//index.html)  \n".format(filepath))
 			# Add the 2d and 3d plots to the main list
-			
 			beta_list.extend(beta_2d_list)
-
 			beta_list.extend(beta_3d_list)
 
 			# Add Alpha, Taxa, and Beta to the master list
 			string_list.extend(alpha_list)
 			string_list.extend(taxa_list)
-			string_list.extend(beta_list)			
-	return string_list
+			string_list.extend(beta_list)
+
+			with open(filepath+'/index.md', 'w') as index_file:
+				for markdown_string in string_list:
+					index_file.write(markdown_string)
+	
+	return list_subdirectories(analysis_folder)
 
 
 def list_subdirectories(parent_dir):
@@ -144,31 +152,38 @@ def list_subdirectories(parent_dir):
 def biom_summary_to_table(secondary_folder):
 	with open(secondary_folder+'/table_summary.txt') as table_file:
 		# Markdown header string 
-		markdown_string = "#### Biom Stats\n```  \n"
+		markdown_string = "#### [Biom Stats](file://{}/table_summary.txt)\n```  \n".format(secondary_folder)
 		for line in table_file:
 			# Get important information
 			# Number of samples
 			if line.startswith('Num samples:'):
-				value_list = line.rstrip().split(': ')
-				markdown_string += "{}			{}\n".format(value_list[0], value_list[1])
+				#value_list = line.rstrip().split(': ')
+				#markdown_string += "{}			{}\n".format(value_list[0], value_list[1])
+				markdown_string += line
 			if line.startswith('Num observations: '):
-				value_list = line.rstrip().split(': ')
-				markdown_string += "{}	{}\n".format(value_list[0], value_list[1])
+				#value_list = line.rstrip().split(': ')
+				#markdown_string += "{}	{}\n".format(value_list[0], value_list[1])
+				markdown_string += line
 			if line.startswith('Total count: '):
-				value_list = line.rstrip().split(': ')
-				markdown_string += "{}			{}\n".format(value_list[0], value_list[1])
+				#value_list = line.rstrip().split(': ')
+				#markdown_string += "{}			{}\n".format(value_list[0], value_list[1])
+				markdown_string += line
 			if line.startswith(' Min: '):
-				value_list = line.rstrip().split(': ')
-				markdown_string += "{}					{}\n".format(value_list[0].lstrip(), value_list[1])
+				#value_list = line.rstrip().split(': ')
+				#markdown_string += "{}					{}\n".format(value_list[0].lstrip(), value_list[1])
+				markdown_string += line
 			if line.startswith(' Max: '):
-				value_list = line.rstrip().split(': ')
-				markdown_string += "{}					{}\n".format(value_list[0].lstrip(), value_list[1])
+				#value_list = line.rstrip().split(': ')
+				#markdown_string += "{}					{}\n".format(value_list[0].lstrip(), value_list[1])
+				markdown_string += line
 			if line.startswith(' Median: '):
-				value_list = line.rstrip().split(': ')
-				markdown_string += "{}				{}\n".format(value_list[0].lstrip(), value_list[1])
+				#value_list = line.rstrip().split(': ')
+				#markdown_string += "{}				{}\n".format(value_list[0].lstrip(), value_list[1])
+				markdown_string += line
 			if line.startswith(' Mean: '):
-				value_list = line.rstrip().split(': ')
-				markdown_string += "{}				{}\n".format(value_list[0].lstrip(), value_list[1])
+				#value_list = line.rstrip().split(': ')
+				#markdown_string += "{}				{}\n".format(value_list[0].lstrip(), value_list[1])
+				markdown_string += line
 	markdown_string += "```   \n"
 	return markdown_string
 
@@ -201,12 +216,30 @@ def main():
 		ERROR = True
 
 	# Write the markdown file
-	with open(analysis_folder+'/index.md', 'w') as markdown:
-		markdown.write(make_header(analysis_folder))
+	with open(analysis_folder+'/index.md', 'w') as markdown_file:
+		# CSS
+		markdown_file.write('<link href="file://{}/github-markdown.css" rel="stylesheet"></link>  \n'.format(analysis_folder))
+		# Make the title
+		markdown_file.write(make_header(analysis_folder))
+		# Make the instructions
+		#markdown_file.write("---  \n###Click on any of the links below to jump to that analysis overview.\n---  \n")
+		# Make the bullet links
 		for bullet in make_secondary_folder_list(analysis_folder):
-			markdown.write(bullet)
-		for secondary_folder_string in make_secondary_folder(analysis_folder):
-			markdown.write(secondary_folder_string)
+			markdown_file.write(bullet)
+	# Make the children folders
+	for secondary_folder in make_secondary_folder_index(analysis_folder):
+			# convert to html
+			markdown_file = analysis_folder+'/'+secondary_folder+'/index.md'
+			html = analysis_folder+'/'+secondary_folder+'/index.html'
+			with open(markdown_file, 'r') as markdown_in, open(html, 'w') as html_out:
+				html_from_markdown = markdown.markdown(markdown_in.read())
+				html_out.write(html_from_markdown)
+	# Convert the main page
+	main_markdown = analysis_folder+'/index.md'
+	main_html =  analysis_folder+'/index.html'
+	with open(main_markdown, 'r') as markdown_in, open(main_html, 'w') as html_out:
+				html_from_markdown = markdown.markdown(markdown_in.read())
+				html_out.write(html_from_markdown)
 
 
 
